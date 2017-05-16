65498492986073fc835d1ae23f92e234efc653a8
Benoit Paternot <benoit.paternot@gmail.com>
2017-05-16 14:55:00 +0200

    Exercice 1 resolu après des heures de galère de fou

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   As a user I can list tasks
+  get 'tasks', to: 'tasks#index'
+# As a user I can display the details of a task
+  get 'tasks/new', to: 'tasks#new', as: "new_task"
+# As a user I can add a task in my ToDo list
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: "edit_task"
+  get 'tasks/:id', to: 'tasks#show', as: "task"
+# As a user I can edit the details of a task
+  patch 'tasks/:id', to: 'tasks#update'
+# As a user I can remove a task from my ToDo list
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
